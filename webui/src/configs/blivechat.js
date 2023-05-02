cf9c37c38295d711883f3b5292ede3b160b0485b
LuiScreaMed <lui5@qq.com>
2023-05-02 16:10:25 +0800

    - 又改了一遍blivechat链接

--- webui/src/configs/blivechat.js
+++ webui/src/configs/blivechat.js
@@ -1,11 +1,11 @@
 /*
  * @Author: LuiScreaMed lui5@qq.com
- * @LastEditTime: 2023-05-02 01:07:16
+ * @LastEditTime: 2023-05-02 16:05:59
  * Copyright (c) 2023 by LuiScreaMed
  * MIT Licensed
  * @Description: link to display danmaku
  */
 
-const bliveLink = "http://localhost:12450/room/213?minGiftPrice=0&minTickerPrice=0.1&showDanmaku=true&showTranslateDanmakuOnly=false&allowTextColorSetting=false&blockTranslateDanmaku=false&translationSign=%E3%80%90&showSuperchat=true&showNewMember=true&showGift=true&showGiftInfo=true&mergeSameUserDanmaku=true&mergeSameUserDanmakuInterval=20&mergeSimilarDanmaku=false&mergeGift=true&danmakuAtBottom=true&tickerAtButtom=false&randomXOffset=false&randomXRangeMin=0&randomXRangeMax=100&randomYOffset=false&randomYRangeMin=250&randomYRangeMax=500&randomXInitialOffset=0&randomYInitialOffset=0&floatDistanceXMin=0&floatDistanceXMax=0&floatDistanceYMin=0&floatDistanceYMax=0&floatDistanceThreshold=0&floatTime=60&maxNumber=10&fadeOutNum=3&pinTime=0&useLocalEmoticonSetting=false&autoRenderOfficialEmoji=true&isGreedyMatch=false&isSkipSameImage=false&imageShowType=0&maxEmoji=5&maxImage=1&blockGiftDanmaku=true&blockLevel=0&blockNewbie=false&blockNotMobileVerified=false&blockKeywords=&blockUsers=&blockMedalLevel=0&minDanmakuInterval=400&maxDanmakuInterval=1200&relayMessagesByServer=false&autoTranslate=false&giftUsernamePronunciation=&emoticons=%5B%5D&lang=zh";
+const bliveLink = "http://localhost:12450/room/372206?minGiftPrice=0&minTickerPrice=0.1&showDanmaku=true&showTranslateDanmakuOnly=false&allowTextColorSetting=false&blockTranslateDanmaku=false&translationSign=%E3%80%90&showSuperchat=true&showNewMember=true&showGift=true&showGiftInfo=true&mergeSameUserDanmaku=true&mergeSameUserDanmakuInterval=20&mergeSimilarDanmaku=false&mergeGift=true&danmakuAtBottom=true&tickerAtButtom=false&randomXOffset=false&randomXRangeMin=0&randomXRangeMax=100&randomYOffset=false&randomYRangeMin=250&randomYRangeMax=500&randomXInitialOffset=0&randomYInitialOffset=0&floatDistanceXMin=0&floatDistanceXMax=0&floatDistanceYMin=0&floatDistanceYMax=0&floatDistanceThreshold=0&floatTime=60&maxNumber=10&fadeOutNum=3&pinTime=0&useLocalEmoticonSetting=false&autoRenderOfficialEmoji=true&isGreedyMatch=false&isSkipSameImage=false&imageShowType=0&maxEmoji=5&maxImage=1&blockGiftDanmaku=true&blockLevel=0&blockNewbie=false&blockNotMobileVerified=false&blockKeywords=&blockUsers=&blockMedalLevel=0&minDanmakuInterval=400&maxDanmakuInterval=1200&relayMessagesByServer=false&autoTranslate=false&giftUsernamePronunciation=&emoticons=%5B%5D&lang=zh";
 
 export default bliveLink;
